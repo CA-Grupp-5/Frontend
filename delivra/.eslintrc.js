9d98397149dd46c8d63a5bc9db7441441973f8f6
alessandro <ales996@yahoo.it>
2025-08-24 23:14:04 +0200

    boiler plate setup, (next: more to add, loosen up linter settings)

---
+++ delivra/.eslintrc.js
@@ -0,0 +1,33 @@
+// https://docs.expo.dev/guides/using-eslint/
+// make this more loose later
+module.exports = {
+  extends: ['expo', 'prettier'],
+  plugins: ['prettier', 'perfectionist', 'unused-imports'],
+  rules: {
+    'perfectionist/sort-imports': ['error'],
+    'perfectionist/sort-interfaces': ['error'],
+    'perfectionist/sort-objects': [
+      'error',
+      {
+        type: 'alphabetical',
+      },
+    ],
+    'prettier/prettier': 'error',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+        vars: 'all',
+        varsIgnorePattern: '^_',
+      },
+    ],
+  },
+  settings: {
+    perfectionist: {
+      partitionByComment: true,
+      type: 'line-length',
+    },
+  },
+};
